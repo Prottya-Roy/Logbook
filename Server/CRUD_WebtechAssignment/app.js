f2e2542f6e4ef5e766b1ebe84b08c8e7553c87d1
Prottya-Roy <61610931+Prottya-Roy@users.noreply.github.com>
2022-07-14 15:05:41 +0600

    body-parser to cors update

--- Server/CRUD_WebtechAssignment/app.js
+++ Server/CRUD_WebtechAssignment/app.js
@@ -2,27 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const userRoutes = require('./api/routes/user');
 const blogRoutes = require('./api/routes/blog');
 const req = require('express/lib/request');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(bodyParser.json());
-
-app.use((rq,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type,Accept, Authorization"
-    );
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+app.use(cors());
+app.use(express.json());
 
 const catchAsync = fn =>(req,res,next)=>{
     Promise.resolve(fn(req,res,next)).catch((error)=>{
